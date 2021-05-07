e934b97b9c15941bc6f7af2acebc76543598ec01
Petcu Dragos <dyenigma@gmail.com>
2021-05-07 22:03:48 +0300

    some updates

--- contracts/Assets.sol
+++ contracts/Assets.sol
@@ -5,35 +5,61 @@ import "./Ownable.sol";
 
 contract Assets is Ownable{
    
-   event NewAsset(string name, string game, string asset_type);
+   event NewAsset(string name, string game, string asset_type, uint cost);
    
    struct Asset{
        string name;
        string game;
        string asset_type;
+       uint cost;
+       bool forSale;
    }
    
    Asset[] public asset_list;
    
-   mapping (uint => address) public assetToOwner;
+   mapping (uint => address) assetToOwner;
    mapping (address => uint) ownerAssetCount;
    
-   function _createAsset(string memory name, string memory game, string memory asset_type) internal {
-        asset_list.push(Asset(name,game,asset_type));
+   function _createAsset(string memory name, string memory game, string memory asset_type, uint cost) public onlyOwner {
+        asset_list.push(Asset(name,game,asset_type,cost,true));
         uint id = asset_list.length - 1;
         assetToOwner[id] = msg.sender;
         ownerAssetCount[msg.sender]++;
-        emit NewAsset(name,game,asset_type);
+        emit NewAsset(name,game,asset_type,cost);
     }
     
-    function showMessage() pure public returns(uint){
-        return 5;
+    function getOwnerFromAssetId(uint256 id) external view returns (address){
+        return assetToOwner[id];
+    }
+    
+    function getOwnerAssetCount(address ownerAdress) external view returns (uint256){
+        return ownerAssetCount[ownerAdress];
+    }
+    
+    function changeOwnerForAsset(uint256 id, address oldOwner, address newOwner) external{
+        assetToOwner[id] = newOwner;
+        ownerAssetCount[oldOwner]--;
+        ownerAssetCount[newOwner]++;
+        asset_list[id].forSale = false;
+    }
+    
+    function setForSale(uint asset_id, bool forSale) external {
+        asset_list[asset_id].forSale = forSale;
+    }
+    
+    function getForSale(uint asset_id) external view returns (bool){
+        return asset_list[asset_id].forSale;
+    }
+    
+    function setCost(uint asset_id, uint cost) external {
+        asset_list[asset_id].cost = cost;
+    }
+    
+    function getCost(uint asset_id) external view returns(uint){
+        return asset_list[asset_id].cost;
     }
     
     
-    // function _generateId(string memory _str) private view returns (uint) {
-    //     uint rand = uint(keccak256(abi.encodePacked(_str)));
-    //     return rand % dnaModulus;
-    // }
+    
 
 }
--- contracts/token.sol
+++ contracts/token.sol
@@ -6,20 +6,39 @@ import "./Assets.sol";
 
 contract Token is erc721 {
     
+    Assets assetsContract;
+    uint transferFee;
+    
+    constructor(){
+        transferFee = 0.001;
+    }
+
+  function setAssetsContractAddress(address _address) external onlyOwner {
+    assetsContract = Assets(_address);
+  }
+    
   function balanceOf(address _owner) external view returns (uint256) {
-     return ownerAssetCount[_owner];
+     return assetsContract.getOwnerAssetCount[_owner];
   }
 
   function ownerOf(uint256 _tokenId) external view returns (address) {
-    return assetToOwner[_tokenId];
+    return assetsContract.getOwnerFromAssetId[_tokenId];
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
     // In this case the sender of the token calls the transferFrom function
+    require (assetsContract.getOwnerFromAssetId(_tokenId) == _from);
+    require (assetsContract.getForSale(_tokenId) == true);
+    require (_from != _to);
+    assetsContract.owner().send(transferFee);
+    assetsContract.changeOwnerForAsset(_tokenId, _from, _to);
+    // emit OwnerChanged();  // todo
   }
 
   function approve(address _approved, uint256 _tokenId) external payable {
-    // in this case the owner or the approved receiver of the token calls it.
+        require (assetsContract.getOwnerFromAssetId(_tokenId) == _approved);
+        assetsContract.owner().send(transferFee);
+        emit Approval(msg.sender, _approved, _tokenId);
   }
 
 
